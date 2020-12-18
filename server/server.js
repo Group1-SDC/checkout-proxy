e495662a6fde6337becb2d6ebfdc20b092059eff
Andrew Song <andrewsong61@gmail.com>
2020-12-18 11:55:36 -0800

    Add loader.io verification

--- server/server.js
+++ server/server.js
@@ -6,6 +6,13 @@ const router = require('./router');
 const PUBLIC_DIR = path.resolve(__dirname, '..', 'public');
 const app = express();
 
+app.use('/', express.static('public'));
+
+// Loader.io verification
+app.get('/loaderio-0d42b9b6193f3171643aff22c8aa776d.txt', (req, res) => {
+  res.send('loaderio-0d42b9b6193f3171643aff22c8aa776d');
+});
+
 app.use('/:id', express.static(PUBLIC_DIR));
 
 // Handling asset requests for webpack bundles by passing off requests to the bundles router
